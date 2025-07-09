f101476854e92b5c06a782c457a5f1c7a19fe0f4
rediet <rb.rediet@gmail.com>
2025-07-09 12:23:05 +0300

    add dependencies and integrate ModeWatcher component in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
 	import '../app.css';
 
+	import { ModeWatcher } from 'mode-watcher';
+
 	let { children } = $props();
 </script>
 
+<ModeWatcher />
+
+<div class="flex h-screen w-full flex-col items-center justify-center gap-8">
+	<div class="absolute top-4 left-4 flex items-center gap-2">
+		<img src="favicon.svg" width="32" height="32" alt="" />
+	</div>
 {@render children()}
