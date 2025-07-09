5230a1bb3ff1fc8e80d1fd1d3e08be4c2180a7c1
rediet <rb.rediet@gmail.com>
2025-07-09 12:25:40 +0300

    add font and update style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,9 @@
 
 	import { ModeWatcher } from 'mode-watcher';
 
+	import '@fontsource-variable/inter';
+	import '@fontsource-variable/manrope';
+
 	let { children } = $props();
 </script>
 
@@ -11,5 +14,13 @@
 <div class="flex h-screen w-full flex-col items-center justify-center gap-8">
 	<div class="absolute top-4 left-4 flex items-center gap-2">
 		<img src="favicon.svg" width="32" height="32" alt="" />
+		<span class="font-logo text-xl font-bold tracking-widest">1ce</span>
 	</div>
-{@render children()}
+	{@render children()}
+</div>
+
+<style>
+	:global(body) {
+		font-family: 'Inter Variable', sans-serif;
+	}
+</style>
