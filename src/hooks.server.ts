539410cdf455cc01ec5adb0c425ad0ab62fc1f6d
rediet <rb.rediet@gmail.com>
2025-07-09 20:44:06 +0300

    use environment variable for dashboard URL redirection

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,11 @@
+import { PUBLIC_DASHBOARD_URL } from '$env/static/public';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = ({ event, resolve }) => {
 	// Check if the request does not have a slug
 	if (!event.params.slug) {
 		// Redirect to the app site
-		return Response.redirect('https://app.1ce.dev', 302);
+		return Response.redirect(PUBLIC_DASHBOARD_URL, 302);
 	}
 
 	// Continue with the request handling
